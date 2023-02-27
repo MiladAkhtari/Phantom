9653a6aeedcbd2f0b4d9cf8b8220d5c27dd9a3dc
MiladAkhtari <126047444+MiladAkhtari@users.noreply.github.com>
2023-02-27 11:45:37 -0800

    Update GitHubPracticeTwo.java
    This changes are from upstream git repo

--- src/test/git/GitHubPracticeTwo.java
+++ src/test/git/GitHubPracticeTwo.java
@@ -4,6 +4,10 @@ public class GitHubPracticeTwo {
 	public int mplt(int a, int b) {
 		return a*b;
 	}
+	public int test(int x, int y, String message){
+		System.out.println(message);
+		int z = x+y;
+		return z;
 	}
 
 
